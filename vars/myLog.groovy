8d49b9ccde1655f37f244749f448d84a184b65e6
dmitry <dmitry.trok@gmail.com>
2020-07-26 13:11:17 +0300

    Update and rename log.groovy to myLog.groovy

---
+++ vars/myLog.groovy
@@ -0,0 +1,33 @@
+def log_info(String message) {
+  echo "INFO: ${message}"
+}
+
+def log_warning(String message) {
+  echo "WARNING: ${message}"
+}
+
+def log_error(String message) {
+  echo "ERROR: ${message}"
+}
+
+def call(Closure body) {
+//  def config = [:]
+//  body.resolveStrategy = Closure.DELEGATE_FIRST
+//  body.delegate = config
+    body()
+    echo "Hello from myLog"
+//
+//  switch (config.type) {
+//      case 'info':
+//      log_info config.message
+//      break
+//      case 'warning':
+//      log_warning config.message
+//      break
+//      case 'error':
+//      log_error config.message
+//      break
+//      default:
+//        error 'Unhandled type.'
+//  }
+}
