1403fd47b63495436fa7197713db3c6fef0fce53
dmitry <dmitry.trok@gmail.com>
2020-07-26 13:33:32 +0300

    Update testStep.groovy

--- vars/testStep.groovy
+++ vars/testStep.groovy
@@ -1,3 +1,20 @@
-def call(Map config) {
-     echo "${config.type}: ${config.message}"
+def success(String message) {
+  echo "INFO: ${message}"
+}
+
+def failure(String message) {
+  echo "WARNING: ${message}"
+}
+
+def call(Map config) {     
+     switch (config.type) {
+          case 'success':
+               success config.message
+               break
+          case 'failure':
+               failure config.message
+               break
+          default:
+               echo 'Unhandled type - ${config.type}.'
+     }
 }
